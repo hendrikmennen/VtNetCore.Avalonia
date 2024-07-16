82d6f6bb9d65a316795d9d873708801880c40c23
Hendrik Mennen <hmennen@protop-solutions.com>
2024-07-17 00:52:40 +0200

    invalidate scroll

--- VtNetCore.Avalonia/VirtualTerminalControl.xaml.cs
+++ VtNetCore.Avalonia/VirtualTerminalControl.xaml.cs
@@ -220,8 +220,12 @@ namespace VtNetCore.Avalonia
                 a = b;
                 b = remainder;
             }
+        }
 
-            ;
+        protected override void OnMeasureInvalidated()
+        {
+            base.OnMeasureInvalidated();
+            SetScrollWindow();
         }
 
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
@@ -232,10 +236,6 @@ namespace VtNetCore.Avalonia
             if (_scrollBar == null) throw new NullReferenceException(nameof(_scrollBar));
 
             _scrollBar.Scroll += (o, i) => { SetScroll((int)i.NewValue); };
-
-            EffectiveViewportChanged += (o, i) => SetScrollWindow();
-            Observable.FromEventPattern<EventArgs>(this, nameof(EffectiveViewportChanged)).Take(1)
-                .Subscribe(x => SetScrollWindow());
         }
 
         private void SetScrollWindow()
@@ -574,24 +574,17 @@ namespace VtNetCore.Avalonia
 
                 if (Terminal.Changed)
                 {
-                    ProcessRawText();
                     Terminal.ClearChanges();
 
                     if (oldTopRow != Terminal.ViewPort.TopRow && oldTopRow >= ViewTop)
                         ViewTop = Terminal.ViewPort.TopRow;
 
+                    SetScrollWindow();
                     InvalidateVisual();
                 }
 
                 TerminalIdleSince = DateTime.Now;
             }
-        }
-
-        private void ProcessRawText()
-        {
-            var incoming = Terminal.RawText;
-
-            SetScrollWindow();
         }
 
         private bool BlinkVisible()
@@ -910,6 +903,8 @@ namespace VtNetCore.Avalonia
                 {
                     Connection.SetTerminalWindowSize(columns, rows);
                 }
+                
+                Dispatcher.UIThread.Post(InvalidateMeasure);
             }
         }
 
